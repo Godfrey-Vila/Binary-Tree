665b766ca961c2d641b5b834e28f504dd43e4709
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-14 17:17:58 +0800

    next is making a function for adding in right side

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -15,5 +15,14 @@ class BinarySearchTreeNode:
             return
 
         if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right= BinarySearchTreeNode(data)
 
 
