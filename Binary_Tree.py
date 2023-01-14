604c4cfb30eed2b98d01fd7c5355e4c6d0079386
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-14 17:42:05 +0800

    making a prcedure for the fast search in left side

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -38,7 +38,14 @@ class BinarySearchTreeNode:
         return elements
 
     def search(self, val):
-        if
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                self.left.search(val)
+            else:
+                return False
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
 
